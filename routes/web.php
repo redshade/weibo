8c4f7c1000641c6c9c3d45cb163b7c76f91fa659
haolei8823 <haolei8823@qq.com>
2019-09-26 21:23:07 +0800

    finish user signup

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/about', 'StaticPagesController@about') -> name('about');
 
 // 用户
 Route::get('/signup', 'UsersController@create') -> name('signup');
+Route::resource('users', 'UsersController');
