d64786351bb3714f365e8293a76ee11ec580b142
haolei8823 <haolei8823@qq.com>
2019-10-16 09:48:38 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,9 +31,12 @@ class UsersController extends Controller
         return view('users.create');
     }
 
-    // 个人详情页
+    // 个人页
     public function show(User $user) {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     // 注册
