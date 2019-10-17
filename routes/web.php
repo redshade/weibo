793e9f6e5f190ccb08fec031d639564b82536a45
haolei8823 <haolei8823@qq.com>
2019-10-17 15:03:15 +0800

    关注列表和粉丝列表

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/about', 'StaticPagesController@about') -> name('about');
 Route::get('/signup', 'UsersController@create') -> name('signup');
 
 // 注册邮件验证
-Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+Route::get('/signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 // 用户
 Route::resource('/users', 'UsersController');
@@ -20,10 +20,14 @@ Route::post('/login', 'SessionController@store')->name('login');
 Route::delete('/logout', 'SessionController@destroy') -> name('logout');
 
 // 密码重设
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 // 动态
-Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+Route::resource('/statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+// 关注列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+// 粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
